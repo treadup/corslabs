80600741a7b425b4d4ff9bba02a6d693e76ab44b
Henrik Treadup <henrik.treadup@gmail.com>
2018-10-06 19:24:30 +0200

    Play around with different Access-Control-Allow headers.

--- corslabs.py
+++ corslabs.py
@@ -1,8 +1,11 @@
-from flask import Flask, request
+import json
+from flask import Flask, request, current_app, make_response
 app = Flask(__name__)
 
 API_HOSTNAME = 'maui.peacefulrainforest.org:5000'
 WEBAPP_HOSTNAME = 'kauai.peacefulrainforest.org:5000'
+
+FULL_WEBAPP_HOSTNAME = "http://" + WEBAPP_HOSTNAME
 
 HTML_TEMPLATE = """
 <!doctype HTML>
@@ -13,17 +16,31 @@ HTML_TEMPLATE = """
 <body>
 <h3>Web app</h3>
 <p>This is the web application. The following data was fetched from the api.</p>
-<div id="people" />
+<pre id="people" />
 </body>
 </html>
 """
 
 JAVASCRIPT_CODE = """
-// This is the JavaScript code.
+const apiHostname = 'API_HOSTNAME';
+const apiEndpoint = 'http://' + apiHostname + '/api';
 
-"""
+fetch(apiEndpoint)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(supermanJson) {
+    const el = document.getElementById('people');
 
-@app.route('/')
+    el.innerText = JSON.stringify(supermanJson);
+  })
+  .catch(function() {
+    console.log("Failed to fetch api data.");
+  })
+
+""".replace("API_HOSTNAME", API_HOSTNAME)
+
+@app.route('/', methods=['GET'])
 def index():
     if not 'Host' in request.headers:
         return "Missing Host header."
@@ -35,7 +52,7 @@ def index():
 
     return HTML_TEMPLATE
 
-@app.route('/static/scripts/index.js')
+@app.route('/static/scripts/index.js', methods=['GET'])
 def javascript():
     if not 'Host' in request.headers:
         return "Missing Host header."
@@ -47,7 +64,7 @@ def javascript():
 
     return JAVASCRIPT_CODE
 
-@app.route('/api')
+@app.route('/api', methods=['GET', 'OPTIONS'])
 def api():
     if not 'Host' in request.headers:
         return "Missing Host header."
@@ -57,4 +74,19 @@ def api():
     if host != API_HOSTNAME:
         return "Incorrect hostname", 400
 
-    return {"name": "Superman", "alias": "Clark Kent", "city": "Metropolis"}
+    if request.method == 'OPTIONS':
+        response = current_app.make_default_options_response()
+        response.headers['Access-Control-Allow-Origin'] = FULL_WEBAPP_HOSTNAME # or "*"
+        response.headers['Access-Control-Allow-Methods'] = 'OPTIONS, HEAD, GET'
+        response.headers['Access-Control-Allow-Headers'] = ""
+        return response
+    elif request.method == 'GET':
+        response = make_response(json.dumps({"name": "Superman",
+                                             "alias": "Clark Kent",
+                                             "city": "Metropolis"}))
+        response.headers['Access-Control-Allow-Origin'] = FULL_WEBAPP_HOSTNAME # or "*"
+        response.headers['Access-Control-Allow-Methods'] = 'OPTIONS, HEAD, GET'
+        response.headers['Access-Control-Allow-Headers'] = ""
+        return response
+
+    return "Unsupported method", 405
