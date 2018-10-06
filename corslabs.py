f3f0ed3aa2540cae801f12d7f74b563ceb947fa2
Henrik Treadup <henrik.treadup@gmail.com>
2018-10-06 17:52:17 +0200

    Streamlined application by only having one webapp endpoint and one api endpoint.

--- corslabs.py
+++ corslabs.py
@@ -4,18 +4,24 @@ app = Flask(__name__)
 API_HOSTNAME = 'maui.peacefulrainforest.org:5000'
 WEBAPP_HOSTNAME = 'kauai.peacefulrainforest.org:5000'
 
-def to_html(content):
-    return f"<html>\n\t<body>\n\t\t{content}\t</body>\n</html>"
+HTML_TEMPLATE = """
+<!doctype HTML>
+<html>
+<head>
+  <script src="/static/scripts/index.js"></script>
+</head>
+<body>
+<h3>Web app</h3>
+<p>This is the web application. The following data was fetched from the api.</p>
+<div id="people" />
+</body>
+</html>
+"""
 
-def webapp_index():
-    return to_html("""<p>This host is the app host. This <a href="/view">/view</a>
-                      contains a web page that makes a fetch call to the api.</p>
-                   """)
+JAVASCRIPT_CODE = """
+// This is the JavaScript code.
 
-def api_index():
-    return to_html("""<p>This host is the api host. The api has a single endpoint
-                      called <a href="/api">/api</a>
-                   """)
+"""
 
 @app.route('/')
 def index():
@@ -24,15 +30,13 @@ def index():
 
     host = request.headers['Host']
 
-    if host == API_HOSTNAME:
-        return api_index()
-    elif host == WEBAPP_HOSTNAME:
-        return webapp_index()
-    else:
-        return f'Unknown host: {host}'
+    if host != WEBAPP_HOSTNAME:
+        return "Incorrect hostname", 400
 
-@app.route('/view')
-def view():
+    return HTML_TEMPLATE
+
+@app.route('/static/scripts/index.js')
+def javascript():
     if not 'Host' in request.headers:
         return "Missing Host header."
 
@@ -41,7 +45,7 @@ def view():
     if host != WEBAPP_HOSTNAME:
         return "Incorrect hostname", 400
 
-    return "This is the view"
+    return JAVASCRIPT_CODE
 
 @app.route('/api')
 def api():
@@ -53,4 +57,4 @@ def api():
     if host != API_HOSTNAME:
         return "Incorrect hostname", 400
 
-    return {"name": "Henrik", "city": "Stockholm"}
+    return {"name": "Superman", "alias": "Clark Kent", "city": "Metropolis"}
